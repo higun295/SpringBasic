54523b5f7393cae02ab04cc9b9a3d01ee13c59cc
HEEKWON <higun295@gmail.com>
2024-06-29 00:51:35 +0900

    do Servlet

--- ServletProject/src/main/java/hello/HelloServlet.java
+++ ServletProject/src/main/java/hello/HelloServlet.java
@@ -7,25 +7,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * Servlet implementation class HelloServlet
- */
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public HelloServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+        System.out.println("===> HelloServlet 생성");
     }
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		System.out.println("---> Service() 호출");
 	}
 
 }
