312008d5c1eeb3fb9afb0474b302acb5a04c8548
HEEKWON <higun295@gmail.com>
2024-06-29 23:44:28 +0900

    studying

---
+++ SpringIoCProject/src/main/java/com/example/tv1/TVUser.java
@@ -0,0 +1,12 @@
+package com.example.tv1;
+
+public class TVUser {
+
+	public static void main(String[] args) {
+		SamsungTV tv = new SamsungTV();
+		tv.powerOn();
+		tv.volumeUp();
+		tv.volumeDown();
+		tv.powerOff();
+	}
+}
