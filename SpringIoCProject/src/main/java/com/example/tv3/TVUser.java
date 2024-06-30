de5da21b48ab5153119b2d10ac80cbc2dccc116f
HEEKWON <higun295@gmail.com>
2024-06-30 23:33:54 +0900

    studying

--- SpringIoCProject/src/main/java/com/example/tv3/TVUser.java
+++ SpringIoCProject/src/main/java/com/example/tv3/TVUser.java
@@ -1,13 +1,16 @@
 package com.example.tv3;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class TVUser {
 
 	public static void main(String[] args) {
-		// 컨테이너를 생성한다.
-		BeanContainer container = new BeanContainer();
+		// Spring 컨테이너를 생성한다.
+		GenericXmlApplicationContext container = 
+				new GenericXmlApplicationContext("applicationContext.xml");
 		
 		// 컨테이너로부터 객체를 검색(Lookup)한다.
-		TV tv = (TV) container.getBean(args[0]);
+		TV tv = (TV) container.getBean("tv");
 		tv.powerOn();
 		tv.volumeUp();
 		tv.volumeDown();
