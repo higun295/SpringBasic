0f84b9d161575b28d991f8744c433320427ed154
HEEKWON <higun295@gmail.com>
2024-07-08 23:46:30 +0900

    add studying

--- SpringIoCProject/src/main/java/com/example/tv3/LgTV.java
+++ SpringIoCProject/src/main/java/com/example/tv3/LgTV.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Component;
 @Component("tv")
 public class LgTV implements TV {
 	
-	@Autowired
-	private AppleSpeaker speaker;
+	// 1. Speaker 타입의 객체가 반드시 메모리에 존재해야 한다.
+	// 2. Speaker 타입의 객체는 반드시 유일해야 한다.
+	@Autowired // Type Injection
+	private Speaker speaker;
 	
 	public LgTV() {
 		System.out.println("===> LgTV 생성");
